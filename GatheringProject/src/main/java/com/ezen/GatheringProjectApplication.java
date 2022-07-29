a510d83b801ac618870dce65cf295ba87592f028
Jeong <ver05@naver.com>
2022-07-29 17:56:26 +0900

    ㅇㅇㅇ

--- GatheringProject/src/main/java/com/ezen/GatheringProjectApplication.java
+++ GatheringProject/src/main/java/com/ezen/GatheringProjectApplication.java
@@ -8,6 +8,7 @@ public class GatheringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatheringProjectApplication.class, args);
+	System.out.println("메롱");
 	}
 
 }
