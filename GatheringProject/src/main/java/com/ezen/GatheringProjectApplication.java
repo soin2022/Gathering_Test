4fcfed6ce32e5d2f14e9bd817bc971e5d75eaf2f
seonghyeon <jeongsh8966@gmail.com>
2022-08-01 13:18:26 +0900

    이그노어 업데이트

--- GatheringProject/src/main/java/com/ezen/GatheringProjectApplication.java
+++ GatheringProject/src/main/java/com/ezen/GatheringProjectApplication.java
@@ -9,6 +9,7 @@ public class GatheringProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatheringProjectApplication.class, args);
 	System.out.println("메롱");
+	System.out.println("이그노어 업데이트");
 	}
 
 }
