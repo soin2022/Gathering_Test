6e8fc1872fde2d8e15b0975a57b5e95a8e0c922d
seonghyeon <jeongsh8966@gmail.com>
2022-08-01 16:32:45 +0900

    edwr

--- GatheringProject/src/main/java/com/ezen/GatheringProjectApplication.java
+++ GatheringProject/src/main/java/com/ezen/GatheringProjectApplication.java
@@ -9,9 +9,8 @@ public class GatheringProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatheringProjectApplication.class, args);
 	System.out.println("메롱");
-	System.out.println("이그노어 업데이트");
-	System.out.println("이그노어 업데이트 했다고");
-	System.out.println("이그노어 업데이트 했다고");
+	System.out.println("이그노어 업데이트 마지막");
+	
 	}
 
 }
